8de0f5df2e876711129df0e4c7347564acc182aa
Tori White <toriw22@gmail.com>
2017-08-07 23:37:45 -0700

    working code

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -1,44 +1,58 @@
 var gifs = ["Cute", "Dog", "Hotdog", "Turtle", "Cat Fail"];
 
-function displayGifs() { 
-	var gifInput = $(this).attr("gifName")
+function generateButtons(){
+
+	for (var j = 0; j < gifs.length; j++) { 
+		var b = $("<button>");
+		b.addClass("gif");
+		b.attr("data-name", gifs[j]);
+		b.text(gifs[j]);
+		$("#buttonsDisplay").append(b);
+	}
+}
+
+generateButtons();
+
+$(".gif").on("click", function(event) {
+
+	$("#gifsDisplay").empty();
+	var gifInput = $(this).data("name");
 
 	var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + gifInput + "&limit=10&rating=pg-13&api_key=e09541ae28964b48a10187d2597e88bf"
-
+	console.log(gifInput);
 	$.ajax({
 		url: giphyURL,
 		method: "GET"
 	}).done(function(response) {
+		
 		console.log(response);
-		var gifDIv = $("<div class='gif'>");
-		var rating = response.rating;
-		var ratingDisplay = $("<h2>").text("Rating: " + rating);
-		gifDiv.append(ratingDisplay);
-		var gifURL = response.url;
-		var image = $("<img>").attr("src", response.data[0].images.downsized_small.original_still.url);
-		gifDiv.append(image);
+		for (var i = 0; i < response.data.length; i++) {
+			var gifDiv = $("<div>");	
+			var rating = response.data[i].rating;
+			console.log(rating);
+			var ratingView = $("<h2>").text("Rating: " + rating);
+			gifDiv.append(ratingView);
+			var gifURL = response.data[i].images.downsized_still.url;
+			var image = $("<img>").attr("src", gifURL);
+			gifDiv.append(image);
 
-		$("#gifsDisplay").append(gifDiv);
-
+			
+			$("#gifsDisplay").append(gifDiv);
+		}
 	});
 
-}
 
-function generateButtons(){
-	for (var i = 0; i < gifs.length; i++) { 
-		var b = $("<button>");
-		b.addClass("gif");
-		b.attr("gifName", gifs[i]);
-		b.text(gifs[i]);
-		$("#gifsDisplay").append(b);
-	}
-}
-
-$("#buttonsDisplay").on("click", function(event) {
-	event.preventDefault();
-	var gifInput = $("#gifInput").val().trim();
-		userInput = userInput.replace(/ /g, "+");
-	gifs.push(gifInput);
+	
 });
 
-generateButtons();
+
+
+$("#addGifButton").on("click", function(event) {
+	event.preventDefault();
+	$("#buttonsDisplay").empty();
+	var gifResponse = $("#gInput").val().trim();
+	var userInput = gifResponse.replace(/ /g, "+");
+	gifs.push(userInput);
+	console.log(gifs);
+	generateButtons();
+});
